b0251782c964a1f818f770309c398134ea3f1c8a
Amber Thompson <amberthompson1@protonmail.com>
2019-09-18 13:19:48 -0400

    Adds zipcode.py

---
+++ zipcode.py
@@ -0,0 +1,26 @@
+from banner import banner
+banner("Zip Code Sorter", "Amber")
+while
+print("Welcome to the Newaygo County zip code sorter.")
+
+zip_code = input("Please enter a zip code: ")
+
+if zip_code == '49309':
+    print(f"The zip code {zip_code} is for Bitely.")
+elif zip_code == '49312':
+    print(f"The zip code {zip_code} is for Brohman.")
+elif zip_code == '49337':
+    print(f"The zip code {zip_code} is for Croton and Newaygo.")
+elif zip_code == '49412':
+    print(f"The zip code {zip_code} is for Fremont.")
+elif zip_code == '49413':
+    print(f"The zip code {zip_code} is for Fremont.")
+elif zip_code == '49327':
+    print(f"The zip code {zip_code} is for Grant.")
+elif zip_code == '49349':
+    print(f"The zip code {zip_code} is for White Cloud.")
+
+else:
+    print(f"{zip_code} is not in Newaygo County")
+
+
